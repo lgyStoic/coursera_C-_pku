d543bc7d331a5c3e66ccef1c9a00e3193a77d1c5
gaoyuanling <lgy6534588@bupt.edu.cn>
2019-07-31 07:51:05 +0800

    add week5  part

--- week5.cpp
+++ week5.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
-
 
 class Solder
 {
@@ -18,8 +18,9 @@ public:
 class Dragon:public Solder {
 private:
     int weapon;
-    float morale;
+    float morale;    
 public:
+    Static int SolderCost;
     Dragon(int no_, int lifeValue_):Solder(no_, lifeValue_){
         weapon = Solder::no % 3;
     }
@@ -30,8 +31,8 @@ class ninja:public Solder {
 private:
     int weapon1;
     int weapon2;
-
 public:
+    Static int SolderCost;
     ninja(int no_, int lifeValue_):Solder(no_, lifeValue_){
         weapon1 = Solder::no % 3;
         weapon2 = (Solder::no + 1) % 3;
@@ -43,6 +44,7 @@ private:
     int weapon;
     float morale;
 public:
+    Static int SolderCost;
     iceman(int no_, int lifeValue_):Solder(no_, lifeValue_){
         weapon = Solder::no % 3;
     }
@@ -52,11 +54,40 @@ class lion:public Solder {
 private:
     int loyalty;
 public:
-    lion(int no_, int lifeValue_):Solder(no_, lifeValue_){
+
+    Static int SolderCost;
+    lion(int no_, int lifeValue_, int loyalty_):Solder(no_, lifeValue_), loyalty(loyalty_){
         
     }
 };
 
 class wolf:public Solder {
+public:
+    Static int SolderCost;
+    wolf(int no_, int lifeValue_):Solder(no_, lifeValue_){
+        
+    }
 
 };
+
+
+class CommanderHouse
+{
+private:
+    int totalLifeValue;
+    string solderOrder[5];
+    int currentIndex;
+public:
+    CommanderHouse(int totalLifeValue_, string[] solderOrder_):totalLifeValue(totalLifeValue_), solderOrder(solderOrder_){
+        currentIndex = 0;
+    }
+
+    ~CommanderHouse();
+
+};
+
+int int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}

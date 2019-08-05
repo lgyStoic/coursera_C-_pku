1dd19ed7cd09b0fe4c51ab0ee802ecce7de2b1c9
gaoyuanling <lgy6534588@bupt.edu.cn>
2019-08-05 23:04:08 +0800

    feat: add week7 part4

--- week7part4.cpp
+++ week7part4.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <string>
+#include <vector>
+using namespace std;
+
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int commandNum;
+    cin >> commandNum;
+    cin.ignore();
+    string *strValue = new string[commandNum];
+    for (int i = 0; i < commandNum; ++i)
+    {
+        getline(cin, strValue[commandNum]);
+    }
+    string cmdstr;
+    vector<string> v;
+    while(cmdstr.compare("over") != 0){
+        getline(cin, cmdstr);
+        stringstream ss(cmdstr);
+        vector<string> substrs;
+        string substr;
+        while()
+
+    }
     return 0;
 }

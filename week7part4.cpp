3f3cf2137dcc0da6a4df9005fee71b3b8b4b3d04
garryling <linggaoyuan@gmail.com>
2019-08-12 09:46:46 +0800

    add unfinish job

--- week7part4.cpp
+++ week7part4.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <list>
+#include <sstream>
 using namespace std;
 
+
+static string opstr[] = {"", };
 
 int main(int argc, char const *argv[])
 {
@@ -15,14 +18,29 @@ int main(int argc, char const *argv[])
         getline(cin, strValue[commandNum]);
     }
     string cmdstr;
-    vector<string> v;
     while(cmdstr.compare("over") != 0){
         getline(cin, cmdstr);
         stringstream ss(cmdstr);
-        vector<string> substrs;
+        list<string> substrslist;
         string substr;
-        while()
-
+        while(ss >> substr){
+            substrslist.push_back(substr);
+        }
+        list<string>::reverse_iterator riter = substrslist.rbegin();
+        while (riter != substrslist.rend())
+        {
+            cout << 1 << endl;
+            if ((*riter).compare("printall") == 0)
+            {
+                for (size_t i = 0; i < commandNum; i++)
+                {
+                    cout << strValue[i] << endl;
+                }
+                
+            }
+            
+        }
+        
     }
     return 0;
 }
